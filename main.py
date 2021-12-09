e8ce2a79c90b14b35566e9916d5611a1bd8fc4ff
OlhaBurtso01 <olhaaa01@gmail.com>
2021-12-10 00:43:32 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def hello():
+    """ Return a friendly HTTP greeting. """
+    return "Hello World!\n"
+
+
+if __name__ == "__main__":
+    # Used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host="localhost", port=8080, debug=True)
